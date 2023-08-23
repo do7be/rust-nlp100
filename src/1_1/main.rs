0087d5d3f380a6c4dbe87af2b973fbe2b3e394f5
do7be <bomsap700@gmail.com>
2023-08-23 22:53:48 +0900

    さらにrefactoring

--- src/1_1/main.rs
+++ src/1_1/main.rs
@@ -1,9 +1,8 @@
 fn main() {
     let str = "パタトクカシーー";
-    let chars = str.chars().collect::<Vec<char>>();
+    let chars = str.chars();
 
     let result = chars
-        .iter()
         .enumerate()
         .filter(|(i, _)| i % 2 == 0)
         .map(|(_, v)| v.to_string())
